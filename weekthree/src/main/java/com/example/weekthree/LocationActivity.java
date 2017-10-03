2fe33299dd72a9c2645acb2e5d2019fd5abd8926
nanorus <nanorus.official@gmail.com>
2017-10-03 14:45:48 +0300

    Google Api Fused Location implemented

--- weekthree/src/main/java/com/example/weekthree/LocationActivity.java
+++ weekthree/src/main/java/com/example/weekthree/LocationActivity.java
@@ -1,40 +1,14 @@
 package com.example.weekthree;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
-
-import static android.view.View.X;
+import butterknife.OnClick;
 
 public class LocationActivity extends AppCompatActivity {
-
-    @BindView(R.id.activity_location_tv_test)
-    TextView tv_test;
-
-    private LocationManager locationManager;
-    StringBuilder location;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,104 +16,21 @@ public class LocationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location);
         ButterKnife.bind(this);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        location = new StringBuilder();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // permission granted
-            requestLocationUpdates();
-        } else {
-            // deny
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
-            }, 0);
+    @OnClick({R.id.activity_location_btn_default, R.id.activity_location_btn_fused})
+    void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.activity_location_btn_default:
+                intent = new Intent(this, LocationDefaultActivity.class);
+                break;
+            case R.id.activity_location_btn_fused:
+                intent = new Intent(this, LocationGoogleActivity.class);
+                break;
         }
-
-
+        startActivity(intent);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        locationManager.removeUpdates(locationListener);
-        tv_test.setText("Отключено");
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 0 && grantResults.length == 2) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                requestLocationUpdates();
-            } else {
-                tv_test.setText("Нет прав");
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-
-    }
-
-    private void requestLocationUpdates() {
-        tv_test.setText("Ищем координаты..");
-        if (locationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER))
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        else {
-            Toast.makeText(this, "Нет провайдера GPS", Toast.LENGTH_SHORT).show();
-        }
-        if (locationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER))
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 60 * 1, 0, locationListener);
-        else
-            Toast.makeText(this, "Нет провайдера NETWORK", Toast.LENGTH_SHORT).show();
-    }
-
-    private LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            Double longitute = location.getLongitude();
-            Double latitude = location.getLatitude();
-            Double altitide = location.getAltitude();
-            LocationActivity.this.location.setLength(0);
-            LocationActivity.this.location
-                    .append("X: ").append(String.valueOf(longitute))
-                    .append("\nY: ").append(String.valueOf(latitude))
-                    .append("\nZ: ").append(String.valueOf(altitide));
-            Geocoder geocoder = new Geocoder(LocationActivity.this, Locale.getDefault());
-            List<Address> addresses = null;
-
-            try {
-                addresses = geocoder.getFromLocation(latitude, longitute, 1);
-                if (addresses.size() > 0) {
-                    LocationActivity.this.location.append(addresses.get(0).getLocality());
-                } else {
-                    LocationActivity.this.location.append("no city");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            tv_test.setText(LocationActivity.this.location);
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-
-        }
-    };
 
 }
--- weekthree/src/main/java/com/example/weekthree/LocationGoogleActivity.java
+++ weekthree/src/main/java/com/example/weekthree/LocationGoogleActivity.java
@@ -1,13 +1,213 @@
 package com.example.weekthree;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LocationGoogleActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class LocationGoogleActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private final int PERMISSION_REQUEST_CODE_DISPLAY_LOCATION = 0;
+    private final int PERMISSION_REQUEST_CODE_START_LOCATION_UPDATES = 1;
+
+    private Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    private TextView lblLocation;
+    private Button btnShowLocation, btnStartLocationUpdates;
+    private boolean mRequestLocationUpdates = false;
+
+    private static int UPDATE_INTERVAL = 10000;
+    private static int FATEST_INTERVAL = 5000;
+    private static int DISPLACEMENT = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_google);
+
+        lblLocation = (TextView) findViewById(R.id.lblLocation);
+        btnShowLocation = (Button) findViewById(R.id.buttonShowLocation);
+        btnStartLocationUpdates = (Button) findViewById(R.id.buttonLocationUpdates);
+
+
+        if (checkPlayServices()) {
+            buildGoogleApiClient();
+            createLocationRequest();
+        }
+
+        btnShowLocation.setOnClickListener(v -> displayLocation());
+        btnStartLocationUpdates.setOnClickListener(v -> togglePeriodLocationUpdates());
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+        if (mGoogleApiClient.isConnected() && mRequestLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        displayLocation();
+
+        if (mRequestLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed: " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+
+        Toast.makeText(getApplicationContext(), "Location changed!", Toast.LENGTH_SHORT).show();
+
+        displayLocation();
+    }
+
+
+    private void displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE_DISPLAY_LOCATION);
+            Toast.makeText(this, "no permission 1", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            double latitude = mLastLocation.getLatitude();
+            double longtitude = mLastLocation.getLongitude();
+
+            lblLocation.setText(latitude + ", " + longtitude);
+        } else {
+            lblLocation.setText("Couldn't get the location. Make sure location is enabled on the device");
+        }
+    }
+
+    private void togglePeriodLocationUpdates() {
+        if (!mRequestLocationUpdates) {
+            btnStartLocationUpdates.setText(getString(R.string.btn_stop_location_updates));
+
+            mRequestLocationUpdates = true;
+
+            startLocationUpdates();
+        } else {
+            btnStartLocationUpdates.setText(getString(R.string.btn_start_location_updates));
+
+            mRequestLocationUpdates = false;
+
+            stopLocationUpdates();
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "This device is not supported", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE_START_LOCATION_UPDATES);
+            Toast.makeText(this, "no permission 2", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE_DISPLAY_LOCATION:
+                displayLocation();
+                break;
+            case PERMISSION_REQUEST_CODE_START_LOCATION_UPDATES:
+                startLocationUpdates();
+                break;
+        }
     }
 }
