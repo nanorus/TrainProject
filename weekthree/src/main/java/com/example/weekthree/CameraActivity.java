beb4ead5ffe7ef540dc84a6bc661547691dccf0c
nanorus <nanorus.official@gmail.com>
2017-10-03 16:57:12 +0300

    Camera runtime permissions implemented

--- weekthree/src/main/java/com/example/weekthree/CameraActivity.java
+++ weekthree/src/main/java/com/example/weekthree/CameraActivity.java
@@ -1,14 +1,18 @@
 package com.example.weekthree;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,8 +22,10 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class CameraActivity extends AppCompatActivity {
-    final int REQUEST_IMAGE_CAPTURE = 1;
-     String APP_DIRECTORY = null;
+
+    final int ACTIVITY_RESULT_REQUEST_IMAGE_CAPTURE = 1;
+    final int PERMISSION_REQUEST_CODE_TAKE_PHOTO = 1;
+    String APP_DIRECTORY = null;
 
     @BindView(R.id.activity_camera_iv_photo)
     ImageView ivPhoto;
@@ -49,7 +55,7 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        if (requestCode == ACTIVITY_RESULT_REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             saveImage(imageBitmap);
@@ -57,10 +63,16 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
+
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                startActivityForResult(takePictureIntent, ACTIVITY_RESULT_REQUEST_IMAGE_CAPTURE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE_TAKE_PHOTO);
+            }
+
         }
     }
 
@@ -86,5 +98,18 @@ public class CameraActivity extends AppCompatActivity {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inPreferredConfig = Bitmap.Config.ARGB_8888;
         return BitmapFactory.decodeFile(APP_DIRECTORY + "avatar.png", options);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE_TAKE_PHOTO:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    dispatchTakePictureIntent();
+                else
+                    Toast.makeText(this, "Lets grand the permission for camera!", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
